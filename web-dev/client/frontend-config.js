fa823481dbff3982ee079906d8cfd77480ce48c1
Sujeet-sharma786 <svishwakarma6389@gmail.com>
2025-03-24 13:31:12 +0530

    'small-change'

--- web-dev/client/frontend-config.js
+++ web-dev/client/frontend-config.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = "https://notepad-vxi7.onrender.com"
+const API_BASE_URL = "https://notepad-vxi7.onrender.com"
 
-const API_BASE_URL = "http://localhost:5000"
+// const API_BASE_URL = "http://localhost:5000"
